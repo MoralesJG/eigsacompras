15cd4449df2b8db069e04abad4a5945e23f73ee7
GabrielM <moralesjimenezgabriel01@gmail.com>
2024-10-02 17:40:14 -0600

    ProveedorControlador: Finalizada

--- src/main/java/com/eigsacompras/controlador/ProveedorControlador.java
+++ src/main/java/com/eigsacompras/controlador/ProveedorControlador.java
@@ -1,27 +1,26 @@
 package com.eigsacompras.controlador;
 
-import com.eigsacompras.basededatos.Conexion;
 import com.eigsacompras.dao.ProveedorDAO;
 import com.eigsacompras.modelo.Proveedor;
 
-import java.sql.Connection;
+import javax.swing.*;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ProveedorControlador {
-    private final ProveedorDAO proveedorDAO;
-    private Connection conexion;
+    private ProveedorDAO proveedorDAO;
 
     public ProveedorControlador(){
-        conexion = Conexion.getConexion();
-        this.proveedorDAO= new ProveedorDAO(conexion);
+        this.proveedorDAO= new ProveedorDAO();
     }
 
     public void agregarProveedor(String nombre, String correo, String telefono, String ubicacion){
         if(validarProveedor(nombre, correo, telefono,ubicacion)){
             Proveedor proveedor = new Proveedor (ubicacion,telefono,nombre,correo);
             proveedorDAO.agregarProveedor(proveedor);
+        }else{
+            JOptionPane.showMessageDialog(null,"Hay uno o más campos vacíos, Revíselos","Campo vacío",JOptionPane.WARNING_MESSAGE);
         }
     }//agregar
 
@@ -33,6 +32,8 @@ public class ProveedorControlador {
         if(validarProveedor(nombre, correo, telefono,ubicacion)){
             Proveedor proveedor = new Proveedor (ubicacion,telefono,nombre,correo,idProveedor);
             proveedorDAO.actualizarProveedor(proveedor);
+        }else {
+            JOptionPane.showMessageDialog(null,"Hay uno o más campos vacíos, Revíselos","Campo vacío",JOptionPane.WARNING_MESSAGE);
         }
     }//actualizar
 
@@ -40,34 +41,20 @@ public class ProveedorControlador {
         proveedorDAO.eliminarProveedor(idProveedor);
     }
 
-
     public boolean validarProveedor(String nombre, String correo, String telefono, String ubicacion){
-        if(nombre.isEmpty()){
-            System.out.println("Nombre no puede estar vacio");
-            return false;
-        }
-        if(!validarCorreo(correo) || correo.isEmpty()){
-            System.out.println("Verifica el correo electronico");
-            return false;
-        }
-        if(telefono.isEmpty()){
-            System.out.println("El telefono no puede estar vacio");
-            return false;
-        }
-        if(ubicacion.isEmpty()){
-            System.out.println("La ubicación no puede estar vacia");
-            return false;
-        }
+        if(nombre.isEmpty()) return false;
+        if(!validarCorreo(correo) || correo.isEmpty()) return false;
+        if(telefono.isEmpty()) return false;
+        if(ubicacion.isEmpty()) return false;
+
         return true;
     }//validar proveedor
-
 
     public boolean validarCorreo(String correo){
         String emailExpresion = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
         Pattern pattern = Pattern.compile(emailExpresion);
         Matcher matcher = pattern.matcher(correo);
-        return matcher.matches();
-
+        return matcher.matches();//retorna true si cumple con lo requerido
     }//validar correo
 
 
