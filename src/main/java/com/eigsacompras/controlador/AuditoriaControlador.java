9f15417f0e86e486bdc9da3764847167b71925c2
GabrielM <moralesjimenezgabriel01@gmail.com>
2024-10-03 17:14:52 -0600

    AuditoriaControlador: Finalizada

---
+++ src/main/java/com/eigsacompras/controlador/AuditoriaControlador.java
@@ -0,0 +1,20 @@
+package com.eigsacompras.controlador;
+
+import com.eigsacompras.dao.AuditoriaDAO;
+import com.eigsacompras.modelo.Auditoria;
+
+import java.util.Date;
+import java.util.List;
+
+public class AuditoriaControlador {
+    private AuditoriaDAO auditoriaDAO;
+
+    public AuditoriaControlador(){
+        auditoriaDAO = new AuditoriaDAO();
+    }
+
+    public List<Auditoria> listarAuditoria(){
+        return auditoriaDAO.listarAuditoria();
+    }//listar
+
+}
